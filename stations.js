4c89450802ec5de1c966865f67a29906a92874ff
GG <45474205+xionggor@users.noreply.github.com>
2026-04-03 21:56:12 -0400

    Update stations.js

--- stations.js
+++ stations.js
@@ -1,33 +1,30 @@
 // ==========================================
-// 🚇 MTA 线路基础配置
+// 🚇 MTA 地铁线路基础配置
 // ==========================================
 const LINE_MAP = {
-    "A": { feed: "ace", color: "#0039A6" }, 
-    "C": { feed: "ace", color: "#0039A6" }, 
-    "E": { feed: "ace", color: "#0039A6" },
-    "B": { feed: "bdfm", color: "#FF6319" }, 
-    "D": { feed: "bdfm", color: "#FF6319" }, 
-    "F": { feed: "bdfm", color: "#FF6319" }, 
-    "M": { feed: "bdfm", color: "#FF6319" },
-    "N": { feed: "nqrw", color: "#FCCC0A", textColor: "black" }, 
-    "Q": { feed: "nqrw", color: "#FCCC0A", textColor: "black" }, 
-    "R": { feed: "nqrw", color: "#FCCC0A", textColor: "black" }, 
-    "W": { feed: "nqrw", color: "#FCCC0A", textColor: "black" },
-    "1": { feed: "1234567", color: "#EE352E" }, 
-    "2": { feed: "1234567", color: "#EE352E" }, 
-    "3": { feed: "1234567", color: "#EE352E" }, 
-    "4": { feed: "1234567", color: "#00933C" }, 
-    "5": { feed: "1234567", color: "#00933C" }, 
-    "6": { feed: "1234567", color: "#00933C" }, 
-    "7": { feed: "1234567", color: "#B933AD" },
-    "L": { feed: "l", color: "#A7A9AC" }, 
-    "G": { feed: "g", color: "#6CBE45" }, 
-    "J": { feed: "jz", color: "#996633" }, 
-    "Z": { feed: "jz", color: "#996633" }
+    "A": { feed: "ace", color: "#0039A6" }, "C": { feed: "ace", color: "#0039A6" }, "E": { feed: "ace", color: "#0039A6" },
+    "B": { feed: "bdfm", color: "#FF6319" }, "D": { feed: "bdfm", color: "#FF6319" }, "F": { feed: "bdfm", color: "#FF6319" }, "M": { feed: "bdfm", color: "#FF6319" },
+    "N": { feed: "nqrw", color: "#FCCC0A", textColor: "black" }, "Q": { feed: "nqrw", color: "#FCCC0A", textColor: "black" }, "R": { feed: "nqrw", color: "#FCCC0A", textColor: "black" }, "W": { feed: "nqrw", color: "#FCCC0A", textColor: "black" },
+    "1": { feed: "1234567", color: "#EE352E" }, "2": { feed: "1234567", color: "#EE352E" }, "3": { feed: "1234567", color: "#EE352E" }, 
+    "4": { feed: "1234567", color: "#00933C" }, "5": { feed: "1234567", color: "#00933C" }, "6": { feed: "1234567", color: "#00933C" }, 
+    "7": { feed: "1234567", color: "#B933AD" }, "L": { feed: "l", color: "#A7A9AC" }, "G": { feed: "g", color: "#6CBE45" }, "J": { feed: "jz", color: "#996633" }, "Z": { feed: "jz", color: "#996633" }
 };
 
 // ==========================================
-// 📍 MTA 车站物理字典
+// 🚌 MTA 巴士地区分类大厅数据
+// ==========================================
+const BUS_BOROUGHS = {
+    "Brooklyn": ["B1", "B4", "B6", "B9", "B15", "B35", "B41", "B44", "B44-SBS", "B46", "B63", "B70", "B82"],
+    "Manhattan": ["M1", "M2", "M3", "M4", "M7", "M14A-SBS", "M14D-SBS", "M15", "M15-SBS", "M34-SBS"],
+    "Queens": ["Q4", "Q5", "Q44-SBS", "Q53-SBS", "Q58", "Q59", "Q88"],
+    "Bronx": ["BX1", "BX2", "BX9", "BX12", "BX12-SBS", "BX41-SBS"],
+    "Staten Island": ["S40", "S44", "S46", "S53", "S79-SBS", "S93", "SIM1", "SIM1C"]
+};
+// 用于搜索框的扁平化全量数据
+const ALL_BUS_ROUTES = [].concat(...Object.values(BUS_BOROUGHS));
+
+// ==========================================
+// 📍 MTA 地铁物理字典
 // ==========================================
 const STATION_DICT = {
     "1": [ 
@@ -35,69 +32,17 @@ const STATION_DICT = {
         { id: "127", name: "34 St-Penn Station", borough: "Manhattan" },
         { id: "132", name: "14 St", borough: "Manhattan" } 
     ],
-    // 🟨 R 线完整版 (Forest Hills - Bay Ridge)
     "R": [
-        { id: "G08", name: "Forest Hills-71 Av", borough: "Queens" },
-        { id: "G09", name: "67 Av", borough: "Queens" },
-        { id: "G10", name: "63 Dr-Rego Park", borough: "Queens" },
-        { id: "G11", name: "Woodhaven Blvd", borough: "Queens" },
-        { id: "G12", name: "Grand Av-Newtown", borough: "Queens" },
-        { id: "G13", name: "Elmhurst Av", borough: "Queens" },
-        { id: "G14", name: "Jackson Hts-Roosevelt Av", borough: "Queens" },
-        { id: "G15", name: "65 St", borough: "Queens" },
-        { id: "G16", name: "Northern Blvd", borough: "Queens" },
-        { id: "G18", name: "46 St", borough: "Queens" },
-        { id: "G19", name: "Steinway St", borough: "Queens" },
-        { id: "G20", name: "36 St", borough: "Queens" },
-        { id: "G21", name: "Queens Plaza", borough: "Queens" },
-        { id: "R11", name: "Lexington Av/59 St", borough: "Manhattan" },
-        { id: "R13", name: "5 Av/59 St", borough: "Manhattan" },
-        { id: "R14", name: "57 St-7 Av", borough: "Manhattan" },
-        { id: "R15", name: "49 St", borough: "Manhattan" },
-        { id: "R16", name: "Times Sq-42 St", borough: "Manhattan" },
-        { id: "R17", name: "34 St-Herald Sq", borough: "Manhattan" },
-        { id: "R18", name: "28 St", borough: "Manhattan" },
-        { id: "R19", name: "23 St", borough: "Manhattan" },
-        { id: "R20", name: "14 St-Union Sq", borough: "Manhattan" },
-        { id: "R21", name: "8 St-NYU", borough: "Manhattan" },
-        { id: "R22", name: "Prince St", borough: "Manhattan" },
-        { id: "R23", name: "Canal St", borough: "Manhattan" },
-        { id: "R24", name: "City Hall", borough: "Manhattan" },
-        { id: "R25", name: "Cortlandt St", borough: "Manhattan" },
-        { id: "R26", name: "Rector St", borough: "Manhattan" },
-        { id: "R27", name: "Whitehall St-South Ferry", borough: "Manhattan" },
-        { id: "R28", name: "Court St", borough: "Brooklyn" },
-        { id: "R29", name: "Jay St-MetroTech", borough: "Brooklyn" },
-        { id: "B08", name: "DeKalb Av", borough: "Brooklyn" },
-        { id: "R31", name: "Atlantic Av-Barclays Ctr", borough: "Brooklyn" },
-        { id: "R32", name: "Union St", borough: "Brooklyn" },
-        { id: "R33", name: "9 St", borough: "Brooklyn" },
-        { id: "R34", name: "Prospect Av", borough: "Brooklyn" },
-        { id: "R35", name: "25 St", borough: "Brooklyn" },
-        { id: "R36", name: "36 St", borough: "Brooklyn" },
-        { id: "R39", name: "45 St", borough: "Brooklyn" },
-        { id: "R40", name: "53 St", borough: "Brooklyn" },
-        { id: "R41", name: "59 St", borough: "Brooklyn" },
-        { id: "R42", name: "Bay Ridge Av", borough: "Brooklyn" },
-        { id: "R43", name: "77 St", borough: "Brooklyn" },
-        { id: "R44", name: "86 St", borough: "Brooklyn" },
+        { id: "G08", name: "Forest Hills-71 Av", borough: "Queens" }, { id: "G14", name: "Jackson Hts-Roosevelt Av", borough: "Queens" },
+        { id: "R11", name: "Lexington Av/59 St", borough: "Manhattan" }, { id: "R16", name: "Times Sq-42 St", borough: "Manhattan" },
+        { id: "R17", name: "34 St-Herald Sq", borough: "Manhattan" }, { id: "R20", name: "14 St-Union Sq", borough: "Manhattan" },
+        { id: "R23", name: "Canal St", borough: "Manhattan" }, { id: "R31", name: "Atlantic Av-Barclays Ctr", borough: "Brooklyn" },
+        { id: "R36", name: "36 St", borough: "Brooklyn" }, { id: "R41", name: "59 St", borough: "Brooklyn" },
         { id: "R45", name: "Bay Ridge-95 St", borough: "Brooklyn" }
     ],
-    "A": [ 
-        { id: "A09", name: "59 St-Columbus Circle", borough: "Manhattan" }, 
-        { id: "A12", name: "W 4 St-Wash Sq", borough: "Manhattan" }, 
-        { id: "A24", name: "Fulton St", borough: "Manhattan" } 
-    ],
-    "7": [
-        { id: "701", name: "Flushing-Main St", borough: "Queens" },
-        { id: "712", name: "74 St-Broadway", borough: "Queens" },
-        { id: "725", name: "Times Sq-42 St", borough: "Manhattan" }
-    ]
+    "A": [ { id: "A09", name: "59 St-Columbus Circle", borough: "Manhattan" }, { id: "A12", name: "W 4 St-Wash Sq", borough: "Manhattan" }, { id: "A24", name: "Fulton St", borough: "Manhattan" } ],
+    "7": [ { id: "701", name: "Flushing-Main St", borough: "Queens" }, { id: "712", name: "74 St-Broadway", borough: "Queens" }, { id: "725", name: "Times Sq-42 St", borough: "Manhattan" } ]
 };
-
-// 补充关联逻辑：为了测试换乘面板，让 N/Q/W 暂时借用 R 线的字典数据
-STATION_DICT["N"] = STATION_DICT["R"];
-STATION_DICT["Q"] = STATION_DICT["R"];
-STATION_DICT["W"] = STATION_DICT["R"];
-STATION_DICT["C"] = STATION_DICT["A"];
-STATION_DICT["E"] = STATION_DICT["A"];
+// 共享字典以测试换乘
+STATION_DICT["N"] = STATION_DICT["R"]; STATION_DICT["Q"] = STATION_DICT["R"]; STATION_DICT["W"] = STATION_DICT["R"];
+STATION_DICT["C"] = STATION_DICT["A"]; STATION_DICT["E"] = STATION_DICT["A"];
